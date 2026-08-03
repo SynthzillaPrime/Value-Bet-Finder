95e498031917d011ddeadb288f811ddad445c02b
SynthzillaPrime <paul4763@gmail.com>
2026-08-03 17:08:10 +0100

    Trim competition list to 33 based on CLV reliability

--- src/constants.ts
+++ src/constants.ts
@@ -33,11 +33,6 @@ export const LEAGUES: LeagueOption[] = [
   { key: "soccer_spl", name: "Scottish Premiership", group: "Top European" },
   { key: "soccer_efl_champ", name: "Championship", group: "Top European" },
   {
-    key: "soccer_spain_segunda_division",
-    name: "La Liga 2",
-    group: "Top European",
-  },
-  {
     key: "soccer_germany_bundesliga2",
     name: "Bundesliga 2",
     group: "Top European",
@@ -56,13 +51,7 @@ export const LEAGUES: LeagueOption[] = [
     name: "Europa League",
     group: "Top European",
   },
-  {
-    key: "soccer_uefa_europa_conference_league",
-    name: "Conference League",
-    group: "Top European",
-  },
   { key: "soccer_fa_cup", name: "FA Cup", group: "Top European" },
-  { key: "soccer_england_efl_cup", name: "EFL Cup", group: "Top European" },
 
   // Other European
   {
@@ -81,23 +70,8 @@ export const LEAGUES: LeagueOption[] = [
     group: "Other European",
   },
   {
-    key: "soccer_finland_veikkausliiga",
-    name: "Veikkausliiga (Finland)",
-    group: "Other European",
-  },
-  {
     key: "soccer_greece_super_league",
     name: "Greek Super League",
-    group: "Other European",
-  },
-  {
-    key: "soccer_league_of_ireland",
-    name: "League of Ireland",
-    group: "Other European",
-  },
-  {
-    key: "soccer_norway_eliteserien",
-    name: "Eliteserien (Norway)",
     group: "Other European",
   },
   {
@@ -111,113 +85,9 @@ export const LEAGUES: LeagueOption[] = [
     group: "Other European",
   },
   {
-    key: "soccer_sweden_superettan",
-    name: "Superettan (Sweden)",
-    group: "Other European",
-  },
-  {
-    key: "soccer_switzerland_superleague",
-    name: "Swiss Superleague",
-    group: "Other European",
-  },
-  {
     key: "soccer_turkey_super_league",
     name: "Turkey Super League",
     group: "Other European",
-  },
-  {
-    key: "soccer_germany_liga3",
-    name: "3. Liga (Germany)",
-    group: "Other European",
-  },
-
-  // Domestic Cups
-  {
-    key: "soccer_italy_coppa_italia",
-    name: "Coppa Italia",
-    group: "Domestic Cups",
-  },
-  {
-    key: "soccer_spain_copa_del_rey",
-    name: "Copa del Rey",
-    group: "Domestic Cups",
-  },
-  {
-    key: "soccer_france_coupe_de_france",
-    name: "Coupe de France",
-    group: "Domestic Cups",
-  },
-  {
-    key: "soccer_germany_dfb_pokal",
-    name: "DFB-Pokal",
-    group: "Domestic Cups",
-  },
-
-  // International
-  {
-    key: "soccer_uefa_nations_league",
-    name: "UEFA Nations League",
-    group: "International",
-  },
-  {
-    key: "soccer_fifa_world_cup_qualifiers_europe",
-    name: "FIFA WCQ Europe",
-    group: "International",
-  },
-  {
-    key: "soccer_fifa_world_cup_qualifiers_south_america",
-    name: "FIFA WCQ South America",
-    group: "International",
-  },
-  {
-    key: "soccer_uefa_euro_qualification",
-    name: "UEFA Euro Qualification",
-    group: "International",
-  },
-  {
-    key: "soccer_africa_cup_of_nations",
-    name: "Africa Cup of Nations",
-    group: "International",
-  },
-  {
-    key: "soccer_concacaf_gold_cup",
-    name: "CONCACAF Gold Cup",
-    group: "International",
-  },
-  {
-    key: "soccer_conmebol_copa_america",
-    name: "Copa América",
-    group: "International",
-  },
-  {
-    key: "soccer_fifa_world_cup",
-    name: "FIFA World Cup",
-    group: "International",
-  },
-  {
-    key: "soccer_fifa_world_cup_womens",
-    name: "FIFA Women's World Cup",
-    group: "International",
-  },
-  {
-    key: "soccer_uefa_european_championship",
-    name: "UEFA Euro",
-    group: "International",
-  },
-  {
-    key: "soccer_fifa_club_world_cup",
-    name: "FIFA Club World Cup",
-    group: "International",
-  },
-  {
-    key: "soccer_uefa_champs_league_qualification",
-    name: "UCL Qualification",
-    group: "International",
-  },
-  {
-    key: "soccer_uefa_champs_league_women",
-    name: "Women's Champions League",
-    group: "International",
   },
 
   // Rest of World
@@ -257,20 +127,7 @@ export const LEAGUES: LeagueOption[] = [
     name: "Copa Sudamericana",
     group: "Rest of World",
   },
-  { key: "soccer_japan_j_league", name: "J League", group: "Rest of World" },
-  { key: "soccer_korea_kleague1", name: "K League 1", group: "Rest of World" },
-  { key: "soccer_mexico_ligamx", name: "Liga MX", group: "Rest of World" },
-  {
-    key: "soccer_saudi_arabia_pro_league",
-    name: "Saudi Pro League",
-    group: "Rest of World",
-  },
   { key: "soccer_usa_mls", name: "MLS", group: "Rest of World" },
-  {
-    key: "soccer_concacaf_leagues_cup",
-    name: "Leagues Cup",
-    group: "Rest of World",
-  },
 ];
 
 // Commission rates used by the SCANNER for edge calculation display.
